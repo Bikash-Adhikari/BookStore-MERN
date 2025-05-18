3ef058d13a94e3eedb0a581f559010e372933413
Bikash-Adhikari <virtual.bikash@gmail.com>
2025-05-18 21:17:14 +0545

    Book Store Web app development:Project

---
+++ FrontEnd/src/components/Banner.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import BannerImg from '../../public/BannerBook.png'
+
+function Banner() {
+    return (
+        <>
+            <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10'>
+                <div className='w-full order-2 md:order-1 mt-10 md:w-1/2 md:mt-30'>
+
+                    <div className='space-y-8'>
+                        <h1 className='text-4xl font-bold'>
+                            Hello, welcome here to learn something <span className='text-pink-500'>new everyday!!!</span>
+                        </h1>
+
+                        <p className='text-md'>
+                            Our passion lies in books that endure. From Austen’s wit and Dickens’s drama to Dostoevsky’s depth and Nietzsche’s daring, Orwell’s insight to Woolf’s lyricism, each title is hand-picked for its power to move you. We scour auctions and private collections worldwide, selecting sweeping epics, incisive essays, and poetry that shaped generations—each edition chosen not merely for its reputation but for the way it brings history alive and sparks conversation today.
+                        </p>
+
+                        <label className="input validator">
+                            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                <g
+                                    strokeLinejoin="round"
+                                    strokeLinecap="round"
+                                    strokeWidth="2.5"
+                                    fill="none"
+                                    stroke="currentColor"
+                                >
+                                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                                </g>
+                            </svg>
+                            <input type="email" placeholder="yourmail@site.com" required />
+                        </label>
+
+                    </div>
+
+                    <button className="btn btn-secondary mt-5">Suscribe</button>
+
+
+                </div>
+
+
+                <div className='w-full order-1 md:w-1/2'>
+                    <img src={BannerImg} className='w-auto h-auto' alt="" />
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+
+export default Banner
