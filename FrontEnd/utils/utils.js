d326c15f3da6bfab51f4e1ca7aeb51363e156c7a
Bikash-Adhikari <virtual.bikash@gmail.com>
2025-05-27 09:38:22 +0545

    Update render-backend url to the frontend base url

--- FrontEnd/utils/utils.js
+++ FrontEnd/utils/utils.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "http://localhost:4001";
+// export const BACKEND_URL = "http://localhost:4001";
+export const BACKEND_URL = "https://bookstore-mern-backend-o223.onrender.com";
